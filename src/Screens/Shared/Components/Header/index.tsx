a7690072d72a2556251b192b54c09df046ebbe0a
furknhelim <furkanhelim.kara28@gmail.com>
2024-04-10 01:41:22 +0300

    ff

--- src/Screens/Shared/Components/Header/index.tsx
+++ src/Screens/Shared/Components/Header/index.tsx
@@ -1,16 +1,34 @@
-import "./index.css"
+import Pages, { IPageListParams } from "./Pages"
+import "./style.css"
+
+const PAGE_LIST: IPageListParams[] = [
+    {
+        title: 'Premium',
+        style: {
+            color: '#FEA013'
+        },
+        onClick: () => {console.log("selam")}
+    },
+    {
+        title: 'Hakkımızda',
+        onClick: () => {}
+    },
+    {
+        title: 'İletişim',
+        onClick: () => {}
+    },
+]
 
 function Header() {
     return (
-        <div className="headerContainer">
+        <div className="header-container">
             <div className="header-left">
-                <img src="../../../../../logo.png" className="header-logo" />
-                <h1 className="header-title">Calorin</h1>
+                <img src="../../../../../logo.png" className="left-logo" />
+                <p className="left-title">Calorin</p>
             </div>
-            <div className="headerScreens">
-                <div>Hakkımızda</div>
-                <div>İletişim</div>
-            </div>
+            <Pages
+                list={PAGE_LIST}
+            />
         </div>
     )
 }
