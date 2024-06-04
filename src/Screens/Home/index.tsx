b7e15826b4298f48a17b38f4b92e4921e4571bce
furknhelim <74779878+furknhelim@users.noreply.github.com>
2024-06-05 01:25:22 +0300

    Update index.tsx

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,11 +1,7 @@
 import { Footer, Header } from '../Shared/Components'
 import './style.css'
-import {useEffect} from "react"
 
 function HomeScreen() {
-    useEffect(() => {
-        fetch("https://sozluk.gov.tr/gts?ara=kalem").then((res) => console.log(res))
-    }, [])
     return (
         <div className="container">
             <Header />
