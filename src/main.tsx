cf5930f4359f2acb3f25f839870673ad69fa8769
furknhelim <furkanhelim.kara28@gmail.com>
2024-07-29 02:57:10 +0300

    ff

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Screens/Home'
 import "./main.css"
-import { BrowserRouter, createBrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Footer, Header } from './Screens/Shared/Components'
 import AboutScreen from './Screens/About'
 
