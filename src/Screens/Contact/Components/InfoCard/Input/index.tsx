6ff428a4c181163f80233d13f1a32124ae724612
furknhelim <furkanhelim.kara28@gmail.com>
2024-10-05 23:13:47 +0300

    ff

--- src/Screens/Contact/Components/InfoCard/Input/index.tsx
+++ src/Screens/Contact/Components/InfoCard/Input/index.tsx
@@ -1,13 +1,14 @@
 import { CSSProperties } from "react"
 import "./index.css"
 
-const ContactInput = ({ style, placeholder, title }: IContactInputProps) => {
+const ContactInput = ({ style, placeholder, title, onChange }: IContactInputProps) => {
     return (
         <div id="contactInput-container" style={style}>
             <div id="contactInput-title">{title}</div>
             <input
                 id="contactInput-input"
                 placeholder={placeholder}
+                onChange={({ target: { value } }) => { onChange(value) }}
             />
         </div>
     )
@@ -17,6 +18,7 @@ interface IContactInputProps {
     style?: CSSProperties
     title: string
     placeholder: string
+    onChange: (text: string) => void
 }
 
 export default ContactInput
--- src/Screens/Contact/Components/InfoCard/index.tsx
+++ src/Screens/Contact/Components/InfoCard/index.tsx
@@ -1,25 +1,58 @@
+import { useTranslation } from "react-i18next"
 import Social from "../../../Shared/Components/Footer/Social"
 import Images from "../../Images"
 import "./index.css"
 import ContactInput from "./Input"
+import { useCallback, useRef } from "react"
 
 function InfoCard() {
+
+    const { t } = useTranslation()
+
+    const values = useRef<IValuesParams>({ email: '', firstName: '', lastName: '', message: '', phone: '' })
+
+    const onSubmit = useCallback(async () => {
+        if (Object.keys(values.current).findIndex((value) => !values.current[value as keyof IValuesParams]) === -1) {
+
+            const formData = new FormData(event.target);
+
+            formData.append("access_key", "6075e7d8-2f5c-4aed-b8a9-50fb11efb263");
+
+            const response = await fetch("https://api.web3forms.com/submit", {
+                method: "POST",
+                body: formData
+            });
+
+            const data = await response.json();
+
+            if (data.success) {
+                setResult("Form Submitted Successfully");
+                event.target.reset();
+            } else {
+                console.log("Error", data);
+                setResult(data.message);
+            }
+
+        } else {
+            alert("Eksik doldurdunuz.")
+        }
+    }, [])
 
     return (
         <div id="infoCard-container">
             <div id="infoCard-left-container">
-                <h2 id="infoCard-left-title">İletişim Bilgilerimiz</h2>
+                <h2 id="infoCard-left-title">{t("page.contact.form.title")}</h2>
                 <div id="infoCard-left-contacts">
                     <div id="infoCard-left-item-title-container" style={{ marginBottom: 45 }}>
                         <img id="infoCard-left-item-image" src={Images.Email} />
-                        <p id="infoCard-left-item-title-main">info@calorinapp.com</p>
+                        <p id="infoCard-left-item-title-main">{t("page.contact.form.email")}</p>
                     </div>
                     <div id="infoCard-left-item-container">
                         <div id="infoCard-left-item-title-container">
                             <img id="infoCard-left-item-image" src={Images.Location} />
-                            <p id="infoCard-left-item-title-main">Tech İstanbul</p>
+                            <p id="infoCard-left-item-title-main">{t("page.contact.form.address.title")}</p>
                         </div>
-                        <p id="infoCard-left-item-title-desc">Merkez, Abide-i Hürriyet Cd No: 211, 34381 Şişli/İstanbul</p>
+                        <p id="infoCard-left-item-title-desc">{t("page.contact.form.address.detail")}</p>
                     </div>
                 </div>
                 <div id="infoCard-left-social">
@@ -29,21 +62,52 @@ function InfoCard() {
             <div id="infoCard-right-container">
                 <div id="infoCard-right-content">
                     <div id="infoCard-right-inputs-row">
-                        <ContactInput title={"Ad"} placeholder={"Bir isim giriniz."} style={{ marginRight: 40 }} />
-                        <ContactInput title={"Soy Ad"} placeholder={"Bir soy isim giriniz."} />
+                        <ContactInput
+                            title={t("page.contact.form.inputs.firstName.title")}
+                            placeholder={t("page.contact.form.inputs.firstName.holder")}
+                            style={{ marginRight: 40 }}
+                            onChange={(text) => { values.current.firstName = text }}
+                        />
+                        <ContactInput
+                            title={t("page.contact.form.inputs.lastName.title")}
+                            placeholder={t("page.contact.form.inputs.lastName.holder")}
+                            onChange={(text) => { values.current.lastName = text }}
+                        />
                     </div>
                     <div id="infoCard-right-inputs-row" style={{ marginTop: 45 }}>
-                        <ContactInput title={"Email"} placeholder={"user@gmail.com"} style={{ marginRight: 40 }} />
-                        <ContactInput title={"İletişim Numarası"} placeholder={"+90 555 555 55 55"} />
+                        <ContactInput
+                            title={t("page.contact.form.inputs.email.title")}
+                            placeholder={t("page.contact.form.inputs.email.holder")}
+                            style={{ marginRight: 40 }}
+                            onChange={(text) => { values.current.email = text }}
+                        />
+                        <ContactInput
+                            title={t("page.contact.form.inputs.phone.title")}
+                            placeholder={t("page.contact.form.inputs.phone.holder")}
+                            onChange={(text) => { values.current.phone = text }}
+                        />
                     </div>
-                    <ContactInput title={"Mesaj"} placeholder={"İletmek istediğiniz metni giriniz."} style={{ marginTop: 45 }} />
-                    <button id="infoCard-right-button" style={{alignSelf: "flex-end"}}>
-                        Gönder
+                    <ContactInput
+                        title={t("page.contact.form.inputs.message.title")}
+                        placeholder={t("page.contact.form.inputs.message.holder")}
+                        style={{ marginTop: 45 }}
+                        onChange={(text) => { values.current.message = text }}
+                    />
+                    <button id="infoCard-right-button" style={{ alignSelf: "flex-end" }} onClick={onSubmit}>
+                        {t("page.contact.form.submitButton")}
                     </button>
                 </div>
             </div>
         </div>
     )
+}
+
+interface IValuesParams {
+    firstName: string
+    lastName: string
+    email: string
+    phone: string
+    message: string
 }
 
 export default InfoCard
